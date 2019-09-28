9692bd5170e12240c4e5a2fac02c3b5fa92b7213
Eternal <konstantinsteinmiller@gmail.com>
2019-09-28 10:59:05 +0200

    project setup with tailwindcss, vue, eslint, pwa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,63 @@
 module.exports = {
-  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+
   env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+
+  plugins: ['prettier', 'node', 'import', 'promise', 'vue'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:node/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:promise/recommended'
   ],
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'spaced-comment': ['error', 'always'],
+    'no-else-return': ['error', { allowElseIf: false }],
+    'no-multi-spaces': ['error', { ignoreEOLComments: false }],
+    'no-multi-str': 'error',
+    'no-undefined': 'error',
+    'handle-callback-err': 'error',
+    'no-path-concat': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'no-alert': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    curly: 'error',
+
+    'prettier/prettier': ['error', { singleQuote: true, semi: false }],
+
+    'promise/no-nesting': ['error'],
+
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] }
+    ]
   },
-  parserOptions: {
-    parser: 'babel-eslint'
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.vue']
+      },
+      alias: {
+        map: [['@', './']],
+        extensions: ['.js', '.vue']
+      }
+    }
   }
 }
