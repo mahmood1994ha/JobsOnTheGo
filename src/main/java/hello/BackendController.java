efeb733df41ffd4d63ebe16ba7fae0ee7091f21b
Mahmoud Bazzal <mahmood1994ha@gmail.com>
2019-09-28 16:22:11 +0200

    WIP backendController

--- src/main/java/hello/BackendController.java
+++ src/main/java/hello/BackendController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +9,12 @@ import backend.interfaces.*;
 import backend.utils.*;
 @RestController
 public class BackendController {
-
+	ArrayList<Job> jobList = new ArrayList<Job>();
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-
+	
 	@RequestMapping("/createjob")
-	public Job greeting(@RequestParam(value="src_lat") double src_lat, 
+	public String greeting(@RequestParam(value="src_lat") double src_lat, 
 							@RequestParam(value="src_long")double src_long,
 							@RequestParam(value="dst_lat")double dst_lat,
 							@RequestParam(value="dst_long")double dst_long,
@@ -23,8 +24,15 @@ public class BackendController {
 							@RequestParam(value="prod_id")String prodID,
 							@RequestParam(value="cons_id")String conID,
 							@RequestParam(value="chan_id")String chanID){
-		return new Job(src_lat,src_long,dst_lat,dst_long,String.format(template, desc),isTokenGiven,tokenCount,
+		String retval = new String();
+		Job x =  new Job(src_lat,src_long,dst_lat,dst_long,String.format(template, desc),isTokenGiven,tokenCount,
 				String.format(template, prodID),String.format(template, conID),String.format(template, chanID));
+		if (x!=null) {
+			jobList.add(x);
+			retval = new String("OK").concat(Integer.toString(jobList.size()));
+		}
+		return retval;
+		
 	}
 }
 
