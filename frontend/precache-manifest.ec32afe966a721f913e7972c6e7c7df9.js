9ad0156f6ca81f4aec1017ea932736994b08d358
Eternal <konstantinsteinmiller@gmail.com>
2019-09-29 09:56:58 +0200

    moving everthing to frontend directory

---
+++ frontend/precache-manifest.ec32afe966a721f913e7972c6e7c7df9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c62ed2be28f0ae0d0977",
+    "url": "/css/app.885bb66d.css"
+  },
+  {
+    "revision": "c62ed2be28f0ae0d0977",
+    "url": "/js/app.9cdd3c7a.js"
+  },
+  {
+    "revision": "be73e8e38c08845c56e5",
+    "url": "/css/chunk-vendors.e8e57780.css"
+  },
+  {
+    "revision": "be73e8e38c08845c56e5",
+    "url": "/js/chunk-vendors.92f6cddb.js"
+  },
+  {
+    "revision": "6d07b43037fb19dfa63ba3fef59a955d",
+    "url": "/img/logo.6d07b430.png"
+  },
+  {
+    "revision": "787f6466978714714c6819f3b7098e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d07b43037fb19dfa63ba3fef59a955d",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
